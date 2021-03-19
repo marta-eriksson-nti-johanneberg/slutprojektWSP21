7f7bc07124bcc0d6a4785da8d93ec2ad79716580
Märta Eriksson <marta.eriksson@elev.ga.ntig.se>
2021-03-19 13:04:34 +0100

    glömt att uppdatera, idek vad jag gjort

--- app.rb
+++ app.rb
@@ -11,14 +11,35 @@ get('/') do
   id = session[:id].to_i
   db = SQLite3::Database.new('db/db.db')
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM cats") # WHERE user_id = ?",id
-  slim(:"index",locals:{cats:result})
+  @result = db.execute("SELECT * FROM cats") # WHERE user_id = ?",id
+  @result2 = db.execute("SELECT * FROM user_cat_relationship WHERE user_id = ?", id)
+  p @result2 
+  slim(:"index")
 end
 
 get('/showlogin') do
   slim(:login)
 end
 
+post('/login') do
+  username = params[:username]
+  password = params[:password]
+  db = SQLite3::Database.new('db/db.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+  pwdigest = result["pwdigest"]
+  id = result["user_id"]
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id 
+    redirect('/')
+  else 
+    "FEL LÖSEN!"
+  end
+end
+
+get('/register') do
+  slim(:register)
+end
 
 begin post('/users/new') do
   username = params[:username]
@@ -29,7 +50,7 @@ begin post('/users/new') do
    
     password_digest = BCrypt::Password.create(password)
     db = SQLite3::Database.new('db/db.db')
-    db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+    db.execute("INSERT INTO users (username,user_role,pwdigest) VALUES (?,?,?)",username,"user",password_digest)
     redirect('/')
   else
     "Lösenorden matchade inte!"
@@ -37,25 +58,20 @@ begin post('/users/new') do
 
 end
 
-post('/login') do
-  username = params[:username]
-  password = params[:password]
+post('/liked/new') do
+  user_id = session[:id]
+  cat_id = params[:cat_id]
   db = SQLite3::Database.new('db/db.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  pwdigest = result["pwdigest"]
-  id = result["id"]
-  if BCrypt::Password.new(pwdigest) == password
-    session[:id] = id 
-    redirect('/')
-  else 
-    "FEL LÖSEN!"
-  end
+  db.execute("INSERT INTO user_cat_relationship (user_id, cat_id) VALUES (?,?)", user_id, cat_id)
+  redirect('/')
 end
 
-
-get('/register') do
-  slim(:register)
+post('/liked/delete') do
+  user_id = session[:id]
+  cat_id = params[:cat_id]
+  db = SQLite3::Database.new('db/db.db')
+  db.execute("DELETE FROM user_cat_relationship WHERE cat_id = ? AND user_id = ?", cat_id, user_id)
+  redirect('/')
 end
 
 post('/todos/delete') do
